d2c312a42f691af56ce6ea2a310aa67b5cbf94c2
Marcus Salinas <markysalinas12@gmail.com>
2023-08-11 12:07:11 +0300

    Some HTML stuff

--- results.php
+++ results.php
@@ -1,9 +1,10 @@
 
-<html>
+<html lang="en">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>Results</title>
 </head>
 
 <body class="container-fluid">
