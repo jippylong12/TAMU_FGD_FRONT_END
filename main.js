46caf8e0ec740ec5ab1db0898618cd80860cbb3e
Marcus Salinas <markysalinas12@gmail.com>
2023-08-12 23:17:39 +0300

    Added all other stuff

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 window.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('course').onclick = function() {
-        gtag('event', 'clicked_')
+    let courseElem = document.getElementById('course');
+    let courseNumberElem = document.getElementById('course_number');
+    let sortByElem = document.getElementById('sort_by');
+    let submitBtnElem = document.getElementById('submit_btn');
+    courseElem.onclick = function() {
+        gtag('event', 'clicked_course');
+    }
+
+    courseNumberElem.onclick = function() {
+        gtag('event', 'clicked_course_number');
+    }
+
+    sortByElem.onclick = function() {
+        gtag('event', 'clicked_sort_by');
+    }
+
+    submitBtnElem.onclick = function() {
+        gtag('event', 'clicked_submit_btn', {
+            course: courseElem.value,
+            course_number: courseNumberElem.value,
+            sort_by: sortByElem.value,
+        });
     }
 })
