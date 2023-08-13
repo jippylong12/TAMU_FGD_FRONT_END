2f4bd4a5845d72731460ea9511b8d40d33945a6d
Marcus Salinas <markysalinas12@gmail.com>
2023-08-13 10:23:59 +0300

    Added code that will take URL as params

--- main.js
+++ main.js
@@ -1,25 +1,44 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
+    setupGtag();
+    addSearchParams()
+})
+
+function addSearchParams() {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    const courseInput = document.getElementById("course");
+    const courseNumberInput = document.getElementById("course_number");
+    const submitBtn = document.getElementById("submit_btn");
+
+    courseInput.value = searchParams.get("course");
+    courseNumberInput.value = searchParams.get("number");
+
+    if (searchParams.get("course") && searchParams.get("number")) {
+        submitBtn.click();
+    }}
+
+function setupGtag() {
     let courseElem = document.getElementById('course');
     let courseNumberElem = document.getElementById('course_number');
     let sortByElem = document.getElementById('sort_by');
     let submitBtnElem = document.getElementById('submit_btn');
-    courseElem.onclick = function() {
+    courseElem.onclick = function () {
         gtag('event', 'clicked_course');
     }
 
-    courseNumberElem.onclick = function() {
+    courseNumberElem.onclick = function () {
         gtag('event', 'clicked_course_number');
     }
 
-    sortByElem.onclick = function() {
+    sortByElem.onclick = function () {
         gtag('event', 'clicked_sort_by');
     }
 
-    submitBtnElem.onclick = function() {
+    submitBtnElem.onclick = function () {
         gtag('event', 'clicked_submit_btn', {
             course: courseElem.value,
             course_number: courseNumberElem.value,
             sort_by: sortByElem.value,
         });
     }
-})
+}
